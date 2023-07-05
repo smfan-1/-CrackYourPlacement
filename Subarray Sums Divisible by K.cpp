51749e55660a667732c2298486bdba23affe0204
smfan-1 <111838351+smfan-1@users.noreply.github.com>
2023-07-05 23:16:31 +0530

    Add files via upload
    45-60 days #CrackYourPlacement challenge #day4 completed

---
+++ Subarray Sums Divisible by K.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int count=0;
+        unordered_map<int,int> mp;
+        int sum=0;
+        mp[0]++;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(mp[(sum%k+k)%k]>0){
+                count+=mp[(sum%k+k)%k];
+            }
+            mp[(sum%k+k)%k]++;
+        }
+        return count;
+    }
+};
