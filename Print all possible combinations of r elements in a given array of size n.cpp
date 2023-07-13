aaa09036ecace92e271e1c5404a22cea6fbe9357
smfan-1 <111838351+smfan-1@users.noreply.github.com>
2023-07-14 00:00:00 +0530

    Add files via upload
    45-60 days #CrackYourPlacement challenge #day7 partial completed

---
+++ Print all possible combinations of r elements in a given array of size n.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void allCpmbination(int arr[],int start,int end,int index){
+    for (int  i = start+1; i < end; i++)
+    {
+        cout<<index<<" "<<arr[i]<<endl;
+    }
+    
+}
+
+int main(){class Solution {
+public:
+    int getLiveNeighbours(vector<vector<int>>& curr, int i, int j, int m, int n) {
+	int liveNeighbours = 0;
+	if(i>0) {
+		if(curr[i-1][j] == 1 || curr[i-1][j] == 3) liveNeighbours++;
+	}
+	if(i<m-1) {
+		if(curr[i+1][j] == 1 || curr[i+1][j] == 3) liveNeighbours++;
+	}
+	if(j>0) {
+		if(curr[i][j-1] == 1 || curr[i][j-1] == 3) liveNeighbours++;
+	}
+	if(j<n-1) {
+		if(curr[i][j+1] == 1 || curr[i][j+1] == 3) liveNeighbours++;
+	}
+
+	if(i>0 && j>0) {
+		if(curr[i-1][j-1] == 1 || curr[i-1][j-1] == 3) liveNeighbours++;
+	}
+	if(i>0 && j<n-1) {
+		if(curr[i-1][j+1] == 1 || curr[i-1][j+1] == 3) liveNeighbours++;
+	}
+	if(i<m-1 && j>0) {
+		if(curr[i+1][j-1] == 1 || curr[i+1][j-1] == 3) liveNeighbours++;
+	}
+	if(i<m-1 && j<n-1) {
+		if(curr[i+1][j+1] == 1 || curr[i+1][j+1] == 3) liveNeighbours++;
+	}
+	return liveNeighbours;
+}
+void gameOfLife(vector<vector<int>>& board) {
+	int m = board.size();
+	int n = board[0].size();
+	for(int i = 0; i<m; i++) {
+		for(int j = 0; j<n; j++) {
+			int ln = getLiveNeighbours(board, i, j, m, n);
+			if(board[i][j] == 0) {
+				if(ln == 3) {
+					board[i][j] = 2; //newly alive
+				}
+			}
+			else if(board[i][j] == 1) {
+				if(ln < 2 || ln > 3) {
+					board[i][j] = 3; //newly dead
+				}
+			}
+		}
+	}
+	for(int i = 0; i<m; i++) {
+		for(int j = 0; j<n; j++) {
+			if(board[i][j] == 2) board[i][j] = 1;
+			else if(board[i][j] == 3) board[i][j] = 0;
+		}
+	}
+}
+};
+    int n;cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    for(int )
+    
+}
